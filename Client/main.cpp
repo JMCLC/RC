d7058c287c1b70bf16cda3723575e3d4e9ff9b80
José Cardoso <61068298+JMCLC@users.noreply.github.com>
2022-12-09 14:23:52 +0000

    Removed old code and updates on play state

--- Client/main.cpp
+++ Client/main.cpp
@@ -10,6 +10,8 @@ int wordSize, errors, currentMove, playerId, fd;
 struct addrinfo hints, *res;
 struct sockaddr_in addr;
 string port, server_ip;
+socklen_t addrlen;
+ssize_t n;
 
 vector<string> stringSplitter(string text) {
     vector<string> res;
@@ -23,30 +25,52 @@ vector<string> stringSplitter(string text) {
 
 vector<string> sendMessageToServer(string message) {
     char buffer[128];
-    ssize_t n = sendto(fd, code, sizeof(code), 0, res->ai_addr, res->ai_addrlen);
+    n = sendto(fd, message, sizeof(message), 0, res->ai_addr, res->ai_addrlen);
     n = recvfrom(fd, buffer, sizeof(buffer), 0, (struct sockaddr*)&addr, &sizeof(addr));
     if (n == -1)
         cout << "Time out" << endl
     return stringSplitter(string(buffer))
 }
 
+void start(string plId) {
+    vector<string> response = sendMessageToServer("SNG " + plId + "\n")
+    playerId = atoi(plId);
+    if (response[1] == "OK") {
+        string wordSizeString;
+        wordSize = atoi(command[2]);
+        errors = atoi(command[3]);
+        for (int i = 0; i < wordSize; i++)
+            wordSizeString.append("_");
+            cout << "New game started (max " << errors << " errors): " << wordSizeString << endl
+    } else
+        cout << "There is already an ongoing game for this player id" << endl;
+}
+
 void play(string letter) {
-    vector<string> response = sendMessageToServer("PLG " + string(playerId) + " " + toLower(letter) + " " + string(currentMove))
+    vector<string> response = sendMessageToServer("PLG " + string(playerId) + " " + toLower(letter) + " " + string(currentMove) + "\n")
     if (response[0] == "RLG") {
         switch(response[1]) {
             case "OK":
+                cout << 'Yes, "' + letter + '" is part of the word: ' << currentState() << endl;
                 break;
             case "WIN":
+                cout << "WELL DONE! You guessed: " << currentState() << endl;
                 break;
             case "DUP":
+                cout << "This letter has already been sent" << endl;
                 break;
             case "NOK":
+                cout << 'No, "' + letter + '" is not part of the word: ' << currentState() << endl;
+                errors -= 1;
                 break;
             case "OVR":
+                cout << "Game over!" << endl;
                 break;
             case "INV":
+
                 break;
             case "ERR":
+
                 break;
         }
     } else
@@ -60,50 +84,27 @@ void handleGame() {
         getline(cin, line);
         command = stringSplitter(line);
         switch(command[0]) {
-            case "play":
-            case "pl":
+            case "start": case "sg":
+                start(command[1]);
+                break;
+            case "play": case "pl":
                 play(command[1]);
                 break;
-            case "guess":
-            case "gw":
+            case "guess": case "gw":
                 break;
-            case "scoreboard":
-            case "sb":
+            case "scoreboard": case "sb":
                 break;
-            case "hint":
-            case "h":
+            case "hint": case "h":
                 break;
-            case "state":
-            case "st":
+            case "state": case "st":
                 break;
-            case "quit":
-            case "exit":
+            case "quit": case "exit":
                 break;
         }
     }
 }
 
-void startGame(int* fd, struct sockaddr_in* addr, struct addrinfo *res, string* plid) {
-    string code = "SNG ";
-    char buffer[12];
-    code.append(string(*plid));
-    ssize_t n = sendto(*fd, code, sizeof(code), 0, *res->ai_addr, *res->ai_addrlen);
-    n = recvfrom(fd, buffer, sizeof(buffer), 0, (struct sockaddr*)&addr, &sizeof(addr));
-    if (n == -1)
-        cout << "Time out" << endl
-    vector<string> sv_response = stringSplitter(string(buffer))
-    if (sv_response[0] == "RSG" && sv_response[1] == "OK") {
-        string wordSizeString;
-        wordSize = stoi(sv_response[2]);
-        errors = stoi(sv_response[3]);
-        for (int i = 0; i < wordSize; i++)
-            wordSizeString.append("_");
-        cout << "New game started (max " << errors << " errors): " << wordSizeString << endl
-        handleGame();
-    } else {
-        cout << "Error" << endl
-    }
-}
+
 
 void main(int argc, char** args) {
     if (argc > 0) {
@@ -137,22 +138,9 @@ void main(int argc, char** args) {
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_DGRAM;
-    errcode = getaddrinfo(server_ip, port, &hints, &res);
-    if (errcode == -1)
-        cout << "Invalid server ip" << endl;
-    getline(cin, message);
-    int i = message.find(" ");
-    plid = message.erase(0, i);
-    playerId = atoi(plid);
-    switch(message) {
-        case 'start':
-        case 'sg':
-            startGame(&fd, &addr, &res, &plid);
-            break;
-        case 'exit':
-            exit(1);
-            break;
-        default:
-            cout << "Error" << endl;
-    }
+    getaddrinfo(server_ip, port, &hints, &res);
+    adddrlen = sizeof(addr);
+    handleGame();
+    freeaddrinfo(res);
+    close(fd);
 }
