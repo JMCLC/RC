57b788fb7c98e94ea3ed8104dc4d16969b3fef36
GMD433 <97476555+GMD433@users.noreply.github.com>
2022-12-20 18:38:04 +0000

    Update main.cpp

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,33 +1,42 @@
 #include <string>
-#include <iostream>
+#include <cstring>
 #include <fstream>
+#include <iostream>
+#include <vector>
 #include <stdio.h>
 #include <sys/socket.h>
 #include <sys/time.h>
-#include <cctype>
-#include <cstring>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <netdb.h>
 #include <unistd.h>
-#include <vector>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <map>
+#include <ctype.h>
 #include <iterator>
 #include <sstream>
 
 using namespace std;
-int wordSize, maxErrors, currentErrors, currentMove, playerId, fd, error;
-struct addrinfo hints, *res;
+
+typedef struct{
+    int currentMove = 1, maxErrors, currentErrors = 0;
+    vector<char> lettersPlayed;
+    string word, currentWord;
+} game;
+
+struct addrinfo hints,*res;
 struct sockaddr_in addr;
-string port, server_ip;
-string currentWord;
+string word_file,S_port;
+map<int,game> gameList;
+int fd, newfd, errcode, verboseMode = 1;
 socklen_t addrlen;
+char buffer[128];
 ssize_t n;
 
 void printArray(vector<string> array) {
     for (int i = 0; i < array.size(); i++) {
-        cout << array[i] << " ";
+        cout << array[i] << endl;
     }
-    cout << endl;
 }
 
 vector<string> stringSplitter(string text) {
@@ -38,238 +47,223 @@ vector<string> stringSplitter(string text) {
     return res;
 }
 
-string toUpper(string text) {
-    for (int i = 0; i < text.length(); i++)
-        text[i] = toupper(text[i]);
-    return text;
-}
-
-vector<string> sendMessageToServer(string message) {
-    string response;
-    char buffer[128];
-    n = sendto(fd, message.c_str(), message.length(), 0, res->ai_addr, res->ai_addrlen);
-    addrlen = sizeof(addr);
-    n = recvfrom(fd, buffer, 128, 0, (struct sockaddr*)&addr, &addrlen);
-    if (n == -1) {
-        cout << "Did not receive a response from the server in time" << endl;
-        return {};
-    }
-    response = buffer;
-    response = response.substr(0, n);
-    return stringSplitter(response);
-}
-
-void printScoreboard(string text) {
-    return;
-}
-
-void printGameState(string text) {
-    return;
-}
-
-void sendMessageToServerTCP(string message) {
-    struct addrinfo tcp_hints;
-    vector<string> response;
-    string currentString, stringBuffer;
-    char buffer[128];
-    ssize_t tcp_n;
-    int tcp_fd = socket(AF_INET, SOCK_STREAM, 0), currentSize = 0, count = 0;
-    tcp_hints.ai_family = AF_INET;
-    tcp_hints.ai_socktype = SOCK_STREAM;
-    tcp_n = connect(tcp_fd, res->ai_addr, res->ai_addrlen);
-    tcp_n = write(tcp_fd, message.c_str(), sizeof(message.c_str()));
-    tcp_n = read(tcp_fd, buffer, 128);
-    stringBuffer = buffer;
-    response = stringSplitter(stringBuffer);
-    if (response[1] == "OK") {
-        ofstream File(response[2]);
-        for (int i = 0; i < sizeof(buffer); i++) {
-            if (buffer[i] == ' ') {
-                count++;
-            }
-            if (count == 4) {
-                currentString = buffer;
-                currentString = currentString.substr(i + 1, sizeof(i + 2));
-                break;
-            }
-        }
-        for (currentSize = sizeof(currentString); currentSize < stoi(response[3]); currentSize += sizeof(currentString)) {
-            File << currentString;
-            if (stoi(response[3]) - currentSize < 128)
-                tcp_n = read(tcp_fd, buffer, stoi(response[3]) - currentSize);
-            else
-                tcp_n = read(tcp_fd, buffer, 128);
-            currentString = buffer; 
-        }
-        File.close();
-    }
-    if (response[0] == "RSB")
-        printScoreboard(response[2]);
-    else if (response[0] == "RHL")
-        cout << "received hint file: " << response[2] << " (" << response[3] << " bytes)" << endl;
-    else if (response[0] == "RST")
-        printGameState(response[2]);
-    close(tcp_n);
-}
-
-void start(string plId) {
-    vector<string> response = sendMessageToServer("SNG " + plId + "\n");
-    cout << "Received: ";
-    printArray(response);
-    if (response.size() == 0)
-        return;
-    playerId = stoi(plId);
-    if (response[1] == "OK") {
-        wordSize = stoi(response[2]);
-        maxErrors = stoi(response[3]);
-        currentMove++;
-        for (int i = 0; i < wordSize; i++)
-            currentWord.append("_ ");
-            cout << "New game started (max " << maxErrors << " errors): " << currentWord << endl;
-    } else
-        cout << "There is already an ongoing game for this player id" << endl;
-}
-
-void play(string letter) {
-    vector<string> response = sendMessageToServer("PLG " + to_string(playerId) + " " + toUpper(letter) + " " + to_string(currentMove) + "\n");
-    cout << "Received: ";
-    printArray(response);
-    if (response.size() == 0)
-        return;
-    if (response[0] == "RLG") {
-        if (response[1] == "OK") {
-            for (int i = 0; i < stoi(response[3]); i++)
-                currentWord[stoi(response[i + 4]) - 1] = toupper(letter[0]);
-            cout << "Yes, " << letter << " is part of the word: " << currentWord << endl;
-            currentMove++;
-        } else if (response[1] == "WIN") {
-            for (int i = 0; i < sizeof(currentWord); i++)
-                if (currentWord[i] == '_')
-                    currentWord[i] = toupper(letter[0]);
-            cout << "WELL DONE! You guessed: " << currentWord << endl;
-            currentMove++;
-        } else if (response[1] == "DUP") {
-            cout << "This letter has already been sent" << endl;
-        } else if (response[1] == "NOK") {
-            cout << "No, " << letter << " is not part of the word" << endl;
-            currentErrors++;
-            currentMove++;
-        } else if (response[1] == "OVR") {
-            cout << "Game over!" << endl;
-            currentMove;
-        } else if (response[1] == "INV") {
-            return;
-        } else if (response[1] == "ERR") {
-            return;
-        }
-    } else
-        cout << "Error" << endl;
-}
-
-void guess(string word) {
-    vector<string> response = sendMessageToServer("PWG " + to_string(playerId) + " " + toUpper(word) + " " + to_string(currentMove) + "\n");
-    cout << "Received: ";
-    printArray(response);
-    if (response.size() == 0)
-        return;
-    if (response[0] == "RWG") {
-        if (response[1] == "WIN") {
-            currentWord = word;
-            cout << "WELL DONE! You guessed: " << currentWord << endl;
-            currentMove++;            
-        } else if (response[1] == "NOK") {
-            cout << "No, " << word << " is not the word";
-            currentErrors++;
-            currentMove++;
-        } else if (response[1] == "OVR") {
-            cout << "Game over!" << endl;
-            currentMove;
-        } else if (response[1] == "INV") {
-            return;
-        } else if (response[1] == "ERR") {
-            return;
-        }
-    }
-}
-
-void scoreboard() {
-    return;
-}
-
-void hint() {
-    return;
-}
-
-void state() {
-    return;
-}
-
-void quit() {
-    exit(1);
-    return;
-}
-
-void handleGame() {
-    vector<string> command = {""};
+string random_word() {
     string line;
-    while (command[0] != "exit") {
-        getline(cin, line);
-        if (line.length() == 0)
-            continue;
-        command = stringSplitter(line);
-        if (command[0] == "start" || command[0] == "sg")
-            start(command[1]);
-        else if (command[0] == "play" || command[0] == "pl")
-            play(command[1]);
-        else if (command[0] == "guess" || command[0] == "gw") 
-            guess(command[1]);
-        else if (command[0] == "scoreboard" || command[0] == "sb")
-            scoreboard();
-        else if (command[0] == "hint" || command[0] == "h")
-            hint();
-        else if (command[0] == "state" || command[0] == "st")
-            state();
-        else if (command[0] == "quit" || command[0] == "exit")
-            quit();
-        else
-            cout << "This is not a valid command!" << endl;
+    vector<string> lines;
+    srand(time(0));
+    ifstream file(word_file);
+    
+    int total_lines=0;
+    while(getline(file,line)) {
+        total_lines++; 
+        lines.push_back(line);  
+    }
+   
+    int random_number=rand()%total_lines;
+    string rw = lines[random_number];
+
+    return rw;
+}
+
+string start(string plId){
+    string response;
+    game newGame;
+    if (gameList.find(stoi(plId)) != gameList.end())
+        return "RSG NOK\n";
+    newGame.word = random_word();    
+    for (int i = 0; i < newGame.word.length() - 1; i++)
+        newGame.currentWord.append("_");
+    if (newGame.word.length() <= 6)
+        newGame.maxErrors = 7;
+    else if(newGame.word.length() <= 10)
+        newGame.maxErrors = 8;
+    else 
+        newGame.maxErrors = 9;
+    if (verboseMode == 1)
+        cout << "PLID=" << plId << ": new game; word = \"" << newGame.word << "\" ("<< newGame.word.length() << " letters)" << endl;
+    response = ("RSG OK  " + to_string(newGame.word.length()) + " " + to_string(newGame.maxErrors) + "\n");    
+    gameList[stoi(plId)] = newGame;
+    return response;
+}
+
+string play(int plId, string letter, int trial){
+    cout << trial  << gameList[plId].currentMove << endl;
+    if (letter.length() > 1 || gameList.find(plId) == gameList.end()){
+        return "RWG ERR\n"; }
+    else if ((gameList[plId].currentMove - 1 == trial && gameList[plId].lettersPlayed.back() != letter[0])|| trial > gameList[plId].currentMove || trial < gameList[plId].currentMove - 1){ 
+        return "RWG INV\n";}
+    for (int i = 0; i < gameList[plId].lettersPlayed.size(); i++)
+        if (gameList[plId].lettersPlayed[i] == letter[0])
+            return "RWG DUP\n";
+    //if (gameList[plId].word.find(letter) == gameList[plId].word.end()) {
+        //gameList[plId].currentErrors++;
+        //if (gameList[plId].currentErrors > gameList[plId].maxErrors) {
+            //gameList.erase(plId);
+            //return "RWG OVR\n";
+        //} else
+    vector<int> positions;
+    int missing;
+    for (int i = 0; i < gameList[plId].word.length(); i++) {;
+        if (letter[0] == gameList[plId].word[i]) 
+            positions.push_back(i + 1);
+            cout << letter[0] << endl;
+        if (gameList[plId].currentWord[i] == *"_")
+            missing++;
+    }
+    if (missing == positions.size()) {
+        //winGame();
+        return "RLG WIN\n";
+    }
+    gameList[plId].currentMove++;
+    string response = ("RLG OK " + to_string(positions.size()) + " ");
+    for (int i = 0; i < positions.size() - 1; i++){ 
+        response.append((to_string(positions[i]) + " "));}
+    cout << "err"<< endl;
+        for (int i = 0; i < gameList[plId].word.length();i++){
+        cout << "err"<< endl;
+        if (*letter.c_str() == gameList[plId].word[i]){
+            break;
+        }
+        response.erase();
+        response = "RLG NOK";
+    }
+    response.append(to_string(positions.back()));
+    return response;
+}
+
+string guess(int plId ,string word,string trial){
+    string response,gameData;
+            fstream file;
+    file.open(*"/home/gd/GS/SCORES/_"+ plId + "_31/12/2022.txt",ios::out);
+    if(!file){
+        cout << "Error in creating file" << endl;
+    }
+    gameData = (plId  + " " + gameList[plId].word + " " + to_string(gameList[plId].currentMove-gameList[plId].currentErrors) + to_string(gameList[plId].currentMove));
+    file << gameData;
+    if (file.is_open()){
+    cout << "Stream could not close!" << endl;
+    }
+    cout << "PLID=" << plId << "guess \"" + word + "\" -WIN (game ended)" << endl;
+    //winGame();
+    return "RWG WIN\n";
+    if (gameList[plId].word == word) {
+        gameList[plId].currentErrors++;
+        if (gameList[plId].currentErrors > gameList[plId].maxErrors) {
+            gameList.erase(plId);
+            return "RWG OVR\n";
+        } else
+            return "RWG NOK\n";
+    }
+    return "ERR\n"; 
+}
+
+string quit(string PLID){
+    if(gameList.find(stoi(PLID)) == gameList.end()){;
+        cout << "PLID=" << PLID << "game ongoing" << endl;
+        return "RQT OK\n";
+    }
+    else{
+        cout << "PLID=" << PLID << "no game found" << endl;
+        return "RQT ERR\n";
     }
 }
 
-int main(int argc, char** args) {
-    if (argc > 0) {
-        if (args[0] == "-n")
-            server_ip = args[1];
-        else if (args[0] == "-p")
-            port = args[1];
+string reveal(string PLID){
+    string response;
+    cout << "PLID=" << PLID << "sending word \"" + gameList[stoi(PLID)].word + "\" to client"<< endl;
+    response =("RRV" + gameList[stoi(PLID)].word + "\n");
+    return response;
+
+}
+
+
+void handleGame(){
+    vector<string> currentCommand;
+    string response, bufferStr;        
+    while(1){
+        addrlen = sizeof(addr);
+        n = recvfrom(fd,buffer,sizeof(buffer),0,(struct sockaddr*)&addr,&addrlen);
+        bufferStr = buffer;
+        bufferStr.substr(0, n);
+        currentCommand = stringSplitter(bufferStr);
+        cout << "Received command: " << currentCommand[0] << endl;
+        if (currentCommand[0] == "SNG")
+            response = start(currentCommand[1]);
+        else if (currentCommand[0] == "PLG"){ 
+            cout << currentCommand[1] << currentCommand[2] << currentCommand[3] << endl;
+            response = play(stoi(currentCommand[1]), currentCommand[2], stoi(currentCommand[3]));}
+        else if(currentCommand[0] == "PWG")
+            response = guess(stoi(currentCommand[1]), currentCommand[2], currentCommand[3]);
+        else if(currentCommand[0] == "QUT")
+            response = quit(currentCommand[1]);
+        else if(currentCommand[0] == "REV")
+            response = reveal(currentCommand[1]);
+        cout << "Sending command: " << response;
+        n = sendto(fd, response.c_str(), response.length(), 0, (struct sockaddr*)&addr, addrlen);
     }
-    if (argc > 2) {
-        if (args[2] == "-n")
-            server_ip = args[3];
-        else if (args[2] == "-p")
-            port = args[3];
+}
+
+bool file_empty(std::ifstream& pFile){
+     return pFile.peek() == std::ifstream::traits_type::eof();
+}
+
+void sendScoreboard(char bufffer[128]){
+    std::ifstream file("/home/gd/GS/SCORES/_top10_scores.txt");
+    if(file_empty(file) == true){
+        bzero(buffer,sizeof(buffer));
+        
     }
-    if (port.length() == 0)
-        port = "58061";
-    if (server_ip.length() == 0)
-        server_ip = "127.0.0.1";
-    cout << "Connecting to server: " << server_ip << ":" << port << endl;
-    struct timeval timeout;
-    timeout.tv_sec = 5;
-    timeout.tv_usec = 0;
-    string message;
-    fd = socket(AF_INET, SOCK_DGRAM, 0);
-    setsockopt(fd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
-    memset(&hints, 0, sizeof hints);
+}
+
+void createTCPconnection(string S_port){
+    string code;
+    fd = socket(AF_INET,SOCK_STREAM,0);
+    if(fd == -1) exit(1);
+
+    memset(&hints,0,sizeof hints);
     hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_DGRAM;
-    error = getaddrinfo(server_ip.c_str(), port.c_str(), &hints, &res);
-    if (error != 0) {
-        cout << "Could not reach the specified server" << endl;
-        return 0;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    errcode = getaddrinfo(NULL,S_port.c_str(),&hints,&res);
+    if((errcode != 0)) exit(1);
+
+    n = bind(fd,res->ai_addr,res->ai_addrlen);
+    if(n==-1) exit(1);
+
+    if(listen(fd,100) == -1) exit(1);
+
+    while(1){
+        addrlen = sizeof(addr);
+        if(newfd =accept(fd,(struct sockaddr*)&addr,&addrlen) == -1) exit(1);
+
+        n = read(newfd,buffer,128);
+        if(n == -1) exit(1);
+
+        //sscanf(buffer,"%s",code);
+        if(code.compare("GSB") == 0){
+            sendScoreboard(buffer);
+        }
     }
+}
+
+int main(int argc, char** args){
+    string tst;
+    word_file = args[1];
+    if(strcmp(args[2],"-p")==0){ 
+        S_port = args[3];
+    } else if(strcmp(args[2],"-v")==0){ 
+        printf("58011\n");
+    }
+    fd=socket(AF_INET,SOCK_DGRAM,0); //UDP socket
+    memset(&hints,0,sizeof hints);
+    hints.ai_family=AF_INET; // IPv4
+    hints.ai_socktype=SOCK_DGRAM; // UDP socket
+    hints.ai_flags=AI_PASSIVE;
+    errcode=getaddrinfo(NULL,S_port.c_str(),&hints,&res);
+    n = bind(fd,res->ai_addr,res->ai_addrlen);
     handleGame();
     freeaddrinfo(res);
     close(fd);
-    return 1;
+    return 0;
 }
